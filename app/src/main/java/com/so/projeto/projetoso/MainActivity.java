ed04714c443cfa5262045b9d080f94166758c14e
Efraim Filho <efraimleite@gmail.com>
2019-03-08 11:09:41 -0300

    Adição do Toast de limite da qtd de aviões

--- app/src/main/java/com/so/projeto/projetoso/MainActivity.java
+++ app/src/main/java/com/so/projeto/projetoso/MainActivity.java
@@ -1,5 +1,6 @@
 package com.so.projeto.projetoso;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -39,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(i);
                 } else {
                         //Colocar dialog informando o erro
+                    // Toast toast = new Toast(getApplicationContext(),"Por favor, digitar menos que 6 voos no solo e 3 voos no ar!",Toast.LENGTH_LONG);
+                    Context context = getApplicationContext();
+                    CharSequence text = "Por favor, digitar até 6 voos no solo e até 3 voos no ar!!";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+//                    toast.show();
                 }
 
             }
